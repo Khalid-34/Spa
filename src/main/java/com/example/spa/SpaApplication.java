b148ff08673dc76f302b74e88ed46d25e497a045
Khalid-34 <77361939+Khalid-34@users.noreply.github.com>
2021-10-06 00:37:44 +0200

    Add files via upload

--- src/main/java/com/example/spa/SpaApplication.java
+++ src/main/java/com/example/spa/SpaApplication.java
@@ -5,28 +5,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 @RestController
 public class SpaApplication {
+	private final static java.util.logging.Logger LOGGER = Logger.getLogger(SpaApplication.class.getName());
 
 	public static void main(String[] args) {
+		LOGGER.setLevel(Level.INFO);
 		SpringApplication.run(SpaApplication.class, args);
 	}
 
 	@GetMapping("/hello")
 	public String hello() {
-
-		return "helloqqqqqqq";
+		LOGGER.info("retour hello");
+		return "BonJour MOI";
 	}
 
 	@GetMapping("/cats")
-	public URL cats() throws MalformedURLException {
+	public BufferedImage cats() throws IOException {
 		Weather weather = new Weather();
 
-		URL a = weather.cats();
+		BufferedImage a = weather.cats();
+		LOGGER.info("retoure le chat");
 		return a;
 	}
 
@@ -35,17 +42,24 @@ public class SpaApplication {
 		Weather weather = new Weather();
 
 		URL a = weather.dog();
+		LOGGER.info("retourne le chien");
 		return a;
 	}
 
 	@GetMapping("/fox")
 	public URL fox() throws MalformedURLException{
 		Weather weather = new Weather();
-
+		LOGGER.info("retourne le renard");
 		URL a = weather.fox();
 		return a;
 	}
 
+//	@GetMapping(value = "/image")
+//	public @ResponseBody byte[] getImage() throws IOException {
+//		InputStream in = getClass()
+//				.getResourceAsStream("C:/Users/utilisateur/Pictures/Saved Pictures/tigre.jpg");
+//		return IOUtils.toByteArray(in);
+//	}
 
 
 }
--- src/main/java/com/example/spa/Weather.java
+++ src/main/java/com/example/spa/Weather.java
@@ -1,12 +1,56 @@
 package com.example.spa;
 
+
+
+import java.util.logging.Logger;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.*;
 
 public class Weather {
 
-    public URL cats() throws MalformedURLException {
-        URL url = new URL("https://aws.random.cat");
-        return url;
+    private final static Logger LOGGER = Logger.getLogger(Weather.class.getName());
+
+    public URL cats() throws IOException {
+        URL url = new URL("https://purr.objects-us-east-1.dream.io/i/2mpW9.jpg");
+        URL url2 = new URL("https://aws.random.cat/meow");
+        LOGGER.info("prend l'url");
+        BufferedImage image = ImageIO.read(url.openStream());
+        //BufferedImage image = ImageIO.read(new URL("https://purr.objects-us-east-1.dream.io/i/2013-10-19%2016.06.51.jpg"));
+        LOGGER.info("prend l'image de L'url");
+//        Image scaledImage = image.getScaledInstance(300, 150, Image.SCALE_DEFAULT);
+        LOGGER.info("prendre la dimension de l'image");
+        ImageIO.write(
+                //convertToBufferedImage(scaledImage),
+                image,
+                "png",
+                new File("C:\\Users\\utilisateur\\Desktop\\image.png"));
+        LOGGER.info("enregistrer l'image au bureaux");
+
+        return url2;
+        //return image;
+    }
+
+    public static BufferedImage convertToBufferedImage(Image img) {
+
+        if (img instanceof BufferedImage) {
+            return (BufferedImage) img;
+        }
+
+        // Create a buffered image with transparency
+        BufferedImage bi = new BufferedImage(
+                img.getWidth(null), img.getHeight(null),
+                BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D graphics2D = bi.createGraphics();
+        graphics2D.drawImage(img, 0, 0, null);
+        graphics2D.dispose();
+
+        return bi;
     }
 
     public URL dog() throws MalformedURLException {
